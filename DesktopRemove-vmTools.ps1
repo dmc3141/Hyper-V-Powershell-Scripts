38a960282ee90e255f8879632af613a90d8e216b
dmc3141 <158590581+dmc3141@users.noreply.github.com>
2025-02-03 13:30:13 -0700

    Add files via upload

---
+++ DesktopRemove-vmTools.ps1
@@ -0,0 +1,18 @@
+﻿Param
+    (
+    [parameter(Mandatory = $true)]
+    [string]$targetVM
+    )
+
+$vmwareToolsGuid =$null
+$regPath = 'HKLM:\\Software\\Microsoft\\Windows\\CurrentVersion\\Uninstall' 
+# Retrieve the GUID of the VMware Tools registry key
+$vmwareToolsGuid = invoke-command -computername $targetVM -scriptblock {Get-ChildItem $using:regPath | Get-ItemProperty | Where-Object {'Vmware Tools' -contains $_.DisplayName }}
+# Uninstall VMware Tools using the GUID 
+if ($vmwareToolsGuid) {
+    Write-Output "Removing VMware Tools"
+    $uninstallstring = $vmwareToolsGuid.pschildname
+    invoke-command -computername $targetVM -scriptblock { cmd.exe /c "msiexec /x $($using:uninstallstring) /qn" }   
+    }
+    
+else { Write-Output "VMware Tools not found." }
